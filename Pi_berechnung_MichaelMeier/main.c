8d7be71f662ba244b67792c2b7ab6885cc69bc15
Michaeldmeier <47794377+Michaeldmeier@users.noreply.github.com>
2019-04-06 14:58:15 +0200

    erster laufender code

--- Pi_berechnung_MichaelMeier/main.c
+++ Pi_berechnung_MichaelMeier/main.c
@@ -2,10 +2,12 @@
  * Pi_berechnung_MichaelMeier.c
  *
  * Created: 20.03.2018 18:32:07
- * Author : chaos
+ * Author : Michael Meier
  */ 
 
 //#include <avr/io.h>
+#include <stdio.h>
+
 #include "avr_compiler.h"
 #include "pmic_driver.h"
 #include "TC_driver.h"
@@ -25,12 +27,16 @@
 #include "utils.h"
 #include "errorHandler.h"
 #include "NHD0420Driver.h"
-
+#include "avr_f64.h"	
+char Pistring[20];
+float *pivar;	
 
 extern void vApplicationIdleHook( void );
-void vLedBlink(void *pvParameters);
+void vDisplayTask(void *pvParameters);
+//void vButtonTask(void *pvParameters);
+void vCalcPiTask(void *pvParameters);
 
-TaskHandle_t ledTask;
+TaskHandle_t PiTask;
 
 void vApplicationIdleHook( void )
 {	
@@ -44,31 +50,74 @@ int main(void)
 	vInitClock();
 	vInitDisplay();
 	
-	xTaskCreate( vLedBlink, (const char *) "ledBlink", configMINIMAL_STACK_SIZE+10, NULL, 1, &ledTask);
+	xTaskCreate( vDisplayTask, (const char *) "Display", configMINIMAL_STACK_SIZE+10, NULL, 2, &PiTask);
+//	xTaskCreate( vButtonTask, (const char *) "Button", configMINIMAL_STACK_SIZE+10, NULL, 1, &PiTask);
+	xTaskCreate( vCalcPiTask, (const char *) "CalcPi", configMINIMAL_STACK_SIZE+300, NULL, 1, &PiTask);
 
 	vDisplayClear();
-	vDisplayWriteStringAtPos(0,0,"FreeRTOS 10.0.1");
-	vDisplayWriteStringAtPos(1,0,"EDUBoard 1.0");
-	vDisplayWriteStringAtPos(2,0,"Template");
-	vDisplayWriteStringAtPos(3,0,"ResetReason: %d", reason);
 	vTaskStartScheduler();
 	return 0;
 }
 
-void vLedBlink(void *pvParameters) {
+void vDisplayTask(void *pvParameters) {
 	(void) pvParameters;
-	PORTF.DIRSET = PIN0_bm; /*LED1*/
+	PORTE.DIRSET = PIN0_bm; /*LED1*/
+	PORTE.OUT = 0x01;
+	for(;;) {
+		PORTE.OUTTGL = 0x01;
+		char* tempResultString = f_to_string(0.5, 16, 16);		//Verwandeln einer Double-Variable in einen String
+		sprintf(Pistring, "1: %s", tempResultString);				//Einsetzen des Strings in einen anderen String
+		vDisplayClear();										//Löschen des ganzen Displays
+		vDisplayWriteStringAtPos(0,0,"Pi Ausgabe");				//Ausgabe auf das Display
+		vDisplayWriteStringAtPos(1,0,"%s", Pistring);
+		vTaskDelay(500 / portTICK_RATE_MS);
+	}
+}
+/*void vButtonTask(void *pvParameters) {
+	(void) pvParameters;
+	PORTF.DIRSET = PIN0_bm; //LED1
 	PORTF.OUT = 0x01;
 	for(;;) {
-// 		uint32_t stack = get_mem_unused();
-// 		uint32_t heap = xPortGetFreeHeapSize();
-// 		uint32_t taskStack = uxTaskGetStackHighWaterMark(ledTask);
-// 		vDisplayClear();
-// 		vDisplayWriteStringAtPos(0,0,"Stack: %d", stack);
-// 		vDisplayWriteStringAtPos(1,0,"Heap: %d", heap);
-// 		vDisplayWriteStringAtPos(2,0,"TaskStack: %d", taskStack);
-// 		vDisplayWriteStringAtPos(3,0,"FreeSpace: %d", stack+heap);
-		PORTF.OUTTGL = 0x01;				
+		PORTF.OUTTGL = 0x01;
 		vTaskDelay(100 / portTICK_RATE_MS);
+	}
+}*/
+void vCalcPiTask(void *pvParameters) {
+	(void) pvParameters;	
+								
+		uint64_t m=0;
+		float64_t n=0;
+		float64_t o=0;
+		float64_t p=0;
+		float64_t q=0;
+		float64_t r=0;
+		float64_t s=0;
+		float64_t t=0;
+		float64_t pii=0;
+		for(;;) 
+		{
+			if(m%2)
+			{
+				n=f_mult(o,f_sd(2));
+				p=f_add(n,f_sd(1));
+				q=f_div(f_sd(4),p);
+				pii=f_sub(pii, q);
+				o=f_add(o, f_sd(1));
+				m++;
+			}
+			else
+			{
+				//h=(-4/(2*m+1));
+				//pivar=f_add(pivar,(float64_t)h);
+				r=f_mult(o,f_sd(2));
+				s=f_add(r,f_sd(1));
+				t=f_div(f_sd(4),s);
+				pii=f_add(pii, t);
+				o=f_add(o, f_sd(1));
+				m++;
+			}
+			//pivar=f_mult(pi4var,f_sd(4));
+		
+	
 	}
 }
